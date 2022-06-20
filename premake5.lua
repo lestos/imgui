8d32ec3fe0c7fa87d24bf723749b6bd3c503ce91
Mateusz <milczak.mateusz@gmail.com>
2022-06-20 20:52:18 +0200

    Premake5 changed

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -8,22 +10,25 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imstb_truetype.h"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-    staticruntime "on"
 
-  filter { "system:windows", "configurations:Release" }
-    buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
